9403231429b281e65ca7d1d5c9e19c87ece07bc1
Dennis.B <Baust.Dennis@gmail.com>
2025-02-06 18:37:42 +0100

    firebase-install

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,5 +1,22 @@
-export interface User {
+export class User {
   id: string;
-  avatar: string;
   name: string;
+  mail: string;
+  password: string;
+
+  constructor(obj?: Partial<User>) {
+    this.id = obj?.id || "";
+    this.name = obj?.name || "";
+    this.mail = obj?.mail || "";
+    this.password = obj?.password || "";
+  }
+
+  public toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      mail: this.mail,
+      password: this.password,
+    };
+  }
 }
---
+++ src/environment/firebase.template.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
