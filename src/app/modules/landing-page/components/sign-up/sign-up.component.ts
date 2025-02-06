e4f8421dd4877c9b50450e95bc9f1ce2652b0d22
Dennis.B <Baust.Dennis@gmail.com>
2025-02-06 02:55:27 +0100

    sign-up-styling

--- src/app/modules/landing-page/components/sign-up/sign-up.component.ts
+++ src/app/modules/landing-page/components/sign-up/sign-up.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { LandingPageComponent } from "../../landing-page.component";
 
 @Component({
-  selector: 'app-sign-up',
-  imports: [],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  selector: "app-sign-up",
+  imports: [FormsModule],
+  templateUrl: "./sign-up.component.html",
+  styleUrl: "./sign-up.component.scss",
 })
 export class SignUpComponent {
+  signUp = inject(LandingPageComponent);
 
+  name = signal<string>("");
+  email = signal<string>("");
+  password = signal<string>("");
+  confirmPassword = signal<string>("");
+  policy = signal<boolean>(false);
+
+  checkPolicy() {
+    this.policy.set(!this.policy());
+  }
+
+  onSubmit() {
+    console.log(
+      this.name(),
+      this.email(),
+      this.password(),
+      this.confirmPassword(),
+    );
+  }
+
+  backToLogIn() {
+    this.signUp.signUp.set(false);
+  }
 }
--- src/app/modules/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page.component.ts
@@ -4,39 +4,47 @@ import {
   ElementRef,
   input,
   OnInit,
+  signal,
   ViewChild,
 } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { User } from "../../core/models/user.model";
 import { LogInComponent } from "./components/log-in/log-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 @Component({
   selector: "app-landing-page",
-  imports: [RouterLink, LogInComponent],
+  imports: [RouterLink, LogInComponent, SignUpComponent],
   templateUrl: "./landing-page.component.html",
   styleUrl: "./landing-page.component.scss",
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
   // user = input.required<User>();
   user = "Test User";
   userId = "1";
 
-  animateLogo: boolean = false; // Trigger logo animation
-  showHeader: boolean = false; // Control header visibility
-  showLogin: boolean = false; // Control login form visibility
-  showLogo: boolean = true;
+  signUp = signal(false);
 
-  ngOnInit() {
-    // Start logo animation after 1 second
-    setTimeout(() => {
-      this.animateLogo = true;
-    }, 1000);
+  viewSignUp() {
+    this.signUp.set(!this.signUp());
   }
 
-  onAnimationEnd() {
-    // After animation ends, display header and login form
-    this.showHeader = true;
-    this.showLogin = true;
-    this.showLogo = false;
-  }
+  // animateLogo: boolean = false; // Trigger logo animation
+  // showHeader: boolean = false; // Control header visibility
+  // showLogin: boolean = false; // Control login form visibility
+  // showLogo: boolean = true;
+
+  // ngOnInit() {
+  //   // Start logo animation after 1 second
+  //   setTimeout(() => {
+  //     this.animateLogo = true;
+  //   }, 1000);
+  // }
+
+  // onAnimationEnd() {
+  //   // After animation ends, display header and login form
+  //   this.showHeader = true;
+  //   this.showLogin = true;
+  //   this.showLogo = false;
+  // }
 }
