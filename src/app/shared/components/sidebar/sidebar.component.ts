53a1f862d954129a75d50a86891a442b5df72365
Dennis.B <Baust.Dennis@gmail.com>
2025-02-10 18:23:42 +0100

    sidebar styling

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,30 @@
 import { Component, inject } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 import { AuthService } from "../../../core/services/auth/auth.service";
 
 @Component({
   selector: "app-sidebar",
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.scss",
 })
 export class SidebarComponent {
   authService = inject(AuthService);
   userId = this.authService.uid;
+
+  summaryPath: string = "assets/img/summary.png";
+  summaryPathActive: string = "assets/img/summary-hover.png";
+  summaryCurrentPath: string = this.summaryPath;
+
+  addTasksPath: string = "assets/img/add-task.png";
+  addTasksPathActive: string = "assets/img/add-task-hover.png";
+  addTasksCurrentPath: string = this.addTasksPath;
+
+  boardPath: string = "assets/img/board.png";
+  boardPathActive: string = "assets/img/board-hover.png";
+  boardCurrentPath: string = this.boardPath;
+
+  contactsPath: string = "assets/img/contacts.png";
+  contactsPathActive: string = "assets/img/contacts-hover.png";
+  contactsCurrentPath: string = this.contactsPath;
 }
