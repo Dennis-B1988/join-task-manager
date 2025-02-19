66c6fba0949dbebec24a1db7414e74e05965fb48
Dennis.B <Baust.Dennis@gmail.com>
2025-02-19 20:09:15 +0100

    greeting message depending on time of day

--- src/app/modules/user-page/components/summary/summary.component.ts
+++ src/app/modules/user-page/components/summary/summary.component.ts
@@ -10,5 +10,19 @@ import { SummaryTaskFormComponent } from "./summary-task-form/summary-task-form.
 })
 export class SummaryComponent {
   authService = inject(AuthService);
+
   userName = computed(() => this.authService.user()?.displayName.split(" ")[0]);
+
+  greetings: string[] = [
+    "Good Day,",
+    "Good Morning,",
+    "Good Afternoon,",
+    "Good Evening,",
+  ];
+
+  timeOfDay: number = Math.floor(
+    (new Date().getHours() / 24) * this.greetings.length,
+  );
+
+  greeting = this.greetings[this.timeOfDay];
 }
