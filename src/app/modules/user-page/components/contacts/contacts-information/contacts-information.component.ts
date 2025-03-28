06917d436ac3b48490d26279d5c7c59b986e512b
Dennis.B <Baust.Dennis@gmail.com>
2025-03-29 00:00:59 +0100

    contact styling

--- src/app/modules/user-page/components/contacts/contacts-information/contacts-information.component.ts
+++ src/app/modules/user-page/components/contacts/contacts-information/contacts-information.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { ContactsService } from "../../../services/contacts/contacts.service";
 
 @Component({
-  selector: 'app-contacts-information',
+  selector: "app-contacts-information",
   imports: [],
-  templateUrl: './contacts-information.component.html',
-  styleUrl: './contacts-information.component.scss'
+  templateUrl: "./contacts-information.component.html",
+  styleUrl: "./contacts-information.component.scss",
 })
 export class ContactsInformationComponent {
+  private contactsService = inject(ContactsService);
 
+  getContactColor(name: string): string {
+    return this.contactsService.generateContactColor(name);
+  }
+
+  getContactInitials(name: string): string {
+    return this.contactsService.getInitials(name);
+  }
 }
