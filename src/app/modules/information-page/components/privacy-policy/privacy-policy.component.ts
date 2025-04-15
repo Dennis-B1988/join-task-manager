d711d95f204cbdce3b7b3bd437b6bd70c36baf19
Dennis.B <Baust.Dennis@gmail.com>
2025-04-15 22:42:13 +0200

    privacy styling

--- src/app/modules/information-page/components/privacy-policy/privacy-policy.component.ts
+++ src/app/modules/information-page/components/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { LandingPageService } from "../../../landing-page/services/landing-page/landing-page.service";
 
 @Component({
-  selector: 'app-privacy-policy',
+  selector: "app-privacy-policy",
   imports: [],
-  templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  templateUrl: "./privacy-policy.component.html",
+  styleUrl: "./privacy-policy.component.scss",
 })
 export class PrivacyPolicyComponent {
+  private landingPageService = inject(LandingPageService);
 
+  goBack() {
+    this.landingPageService.goBackToLogIn();
+  }
 }
