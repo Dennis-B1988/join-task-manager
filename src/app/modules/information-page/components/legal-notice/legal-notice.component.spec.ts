5bb4faff8a58effa934b53c0c461af0e989f7542
Dennis.B <Baust.Dennis@gmail.com>
2025-04-26 19:51:56 +0200

    unit test

--- src/app/modules/information-page/components/legal-notice/legal-notice.component.spec.ts
+++ src/app/modules/information-page/components/legal-notice/legal-notice.component.spec.ts
@@ -1,3 +1,41 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { signal } from "@angular/core";
+import { MockProvider } from "ng-mocks";
+import { LandingPageService } from "../../../landing-page/services/landing-page/landing-page.service";
 import { LegalNoticeComponent } from "./legal-notice.component";
+
+describe("LegalNoticeComponent", () => {
+  let component: LegalNoticeComponent;
+  let fixture: ComponentFixture<LegalNoticeComponent>;
+  let mockService: LandingPageService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LegalNoticeComponent],
+      providers: [
+        MockProvider(LegalNoticeComponent, {
+          legalNoticeActive: signal(false),
+        }),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LegalNoticeComponent);
+    component = fixture.componentInstance;
+    mockService = TestBed.inject(LandingPageService);
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe("computed signals", () => {
+    it("reflects service.legalNoticeActive", () => {
+      expect(component.legalNoticeActive()).toBe(false);
+      mockService.legalNoticeActive.set(true);
+      fixture.detectChanges();
+      expect(component.legalNoticeActive()).toBe(true);
+    });
+  });
+});
--- src/app/modules/information-page/components/privacy-policy/privacy-policy.component.spec.ts
+++ src/app/modules/information-page/components/privacy-policy/privacy-policy.component.spec.ts
@@ -1,23 +1,40 @@
+import { signal } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { MockComponent, MockProvider } from "ng-mocks";
+import { MockProvider } from "ng-mocks";
+import { LandingPageService } from "../../../landing-page/services/landing-page/landing-page.service";
 import { PrivacyPolicyComponent } from "./privacy-policy.component";
 
 describe("PrivacyPolicyComponent", () => {
   let component: PrivacyPolicyComponent;
   let fixture: ComponentFixture<PrivacyPolicyComponent>;
+  let mockService: LandingPageService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PrivacyPolicyComponent],
-      providers: [MockProvider(PrivacyPolicyComponent)],
+      providers: [
+        MockProvider(PrivacyPolicyComponent, {
+          privacyPolicyActive: signal(false),
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PrivacyPolicyComponent);
     component = fixture.componentInstance;
+    mockService = TestBed.inject(LandingPageService);
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  describe("computed signals", () => {
+    it("reflects service.privacyPolicyActive", () => {
+      expect(component.privacyPolicyActive()).toBe(false);
+      mockService.privacyPolicyActive.set(true);
+      fixture.detectChanges();
+      expect(component.privacyPolicyActive()).toBe(true);
+    });
   });
 });
